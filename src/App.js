e5df5ba25c70397226a7f27f2a45474e2eefdbe1
Pradeep Elamurugu <pradeepelamurugu2@gmail.com>
2024-04-26 17:45:15 +0530

    checkpoint 3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./styles.css";
 
 export default function App() {
+  let todo_list = ["Read SpringBoot", "Complete assignments", "Prepare breakfast", "Sleep for 2 hours", "Take a shower"]
   return (
     <div className="Application">
       <h1>Todo App</h1>
-      <h2>Nothing to do buddy. Sleep!!</h2>
+      {todo_list.map((item,index)=><h2 key={index}>{item}</h2>)}
     </div>
   );
 }
---
+++ src/TodoItem.js
@@ -0,0 +1,7 @@
+export default function todo_item({content}){
+    return(
+        <div>
+            <h1>{content}</h1>
+        </div>
+    )
+}
